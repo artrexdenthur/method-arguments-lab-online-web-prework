4d646aa10846d069fb732b0dddbc88d4ccf77e47
Paul Ashour <paul.ashour@gmail.com>
2018-09-30 18:45:31 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(dum1)
 end
 
 def introduction_with_language(dum1, dum2)
-  puts "Hi, my name is Dan and I am learning to program in Ember.js"
+  puts "Hi, my name is Dan and I am learning to program in Ember.js."
 end
